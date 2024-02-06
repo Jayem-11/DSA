e964c31c74e7ba9713bb36b28c0880df092161d4
Jayem-11 <ronojohnmichael@gmail.com>
2024-02-06 10:10:37 +0300

    189_leetcode

---
+++ Leet_code_top_150/189_rotate_array.py
@@ -0,0 +1,11 @@
+def rotate1(nums, k):
+    for j in range(k):
+        temp = nums[len(nums)-1]
+        for i in range(len(nums)-1,-1,-1):
+            if i == 0:
+                nums[i] = temp
+            else:
+                nums[i] = nums[i-1]
+
+def rotate2(self,nums, k):
+        nums[:] = nums[-(k % len(nums)):] + nums[:-(k % len(nums))]
