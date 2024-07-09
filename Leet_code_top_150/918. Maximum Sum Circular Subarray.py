272287a557d2b14b0b60e904089ed3f5edea2a01
Jayem-11 <ronojohnmichael@gmail.com>
2024-07-09 11:31:49 +0300

    leetcode_918

---
+++ Leet_code_top_150/918. Maximum Sum Circular Subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, nums):
+        globMax, globMin = nums[0], nums[0]
+        curMax, curMin = 0, 0
+        total = 0
+
+        for n in nums:
+            curMax = max(n, curMax + n)
+            curMin = min(n, curMin + n)
+
+            total+=n
+            globMax = max(globMax, curMax)
+            globMin = min(globMin, curMin)
+
+    	return max(globMax, total - globMin) if globMax > 0 else globMax
